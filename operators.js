3098bb1ef0f1879a20149b85ee26c2766a91474f
bhukyapriya5171-create <bhukyapriya5171@gmail.com>
2026-02-12 08:55:40 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,40 @@
+let a=10;
+let b=10;
+console.log("a==b",a==b);
+console.log("a===b", a===b);
+console.log ("a!=b",a!=b);
+console.log("a>b:".a>b);
+console.log("a<b:",a<b);
+console.log("a>=b:", a>=b);
+console.log("a<=b:",a<=b);
+// Even or odd number
+let num =2;
+let result =(num%2===0)?"Even":"odd";
+console.log("Number is:",result);
+// Adult or minor
+let userage=15;
+let Agestatus=(userage>=18)?"adult":"minor";
+console.log("Userage is" ,Agestatus)
+// grade
+let Score=95;
+let grade=(Score>=90)? "A":
+          (Score>=75)? "B":
+          (Score>=60)? "C":
+          (Score>=45)? "D":"f";
+          console.log( "Grade:",grade);
+// temperature
+let temperature=50;
+let  tempcategorize= (temperature>30)?"hot":
+                   (temperature>=20)? "warm":
+                   (temperature>=10)? "cool":"cold";
+        console.log("Temperature :" , tempcategorize);
+
+ let  age =30;
+ let  ageGroup = (age >=0 && age<=12)? "child"  :
+                  (age>=19)? "Teen":
+                  (age>64)? "Adult":"Senior";
+       console.log("Age :" ,ageGroup);           
+
+                   
+
+          
